06e6d0e297ed6f1cf14f470332c1815de57178c7
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2022-02-22 18:45:56 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -8,7 +8,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def setup():
     # driver = webdriver.Chrome(executable_path="F:\selemium\chromedriver.exe")
-    s = Service(ChromeDriverManager().install())
+#     s = Service(ChromeDriverManager().install())
+    s = Service('/home/gaurav/Downloads/chromedriver_linux64/chromedriver')
+# driver = webdriver.Chrome(service=s)
     driver = webdriver.Chrome(service=s)
     return driver
 
