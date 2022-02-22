52984d365cfab572bc55f6982d1b3814de7d05a2
Gaurav Singh <Gaurav.Singh@AD.MSYSTECHNOLOGIES.COM>
2022-02-22 12:17:46 +0530

    webdeiver updated

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,9 +1,14 @@
 import pytest
 from selenium import webdriver
 
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture
 def setup():
-    driver = webdriver.Chrome(executable_path="F:\selemium\chromedriver.exe")
+    # driver = webdriver.Chrome(executable_path="F:\selemium\chromedriver.exe")
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
     return driver
 
