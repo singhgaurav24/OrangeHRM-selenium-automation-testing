374875261ac98eb22dbaf579f41e753a3ece9c4d
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2022-03-06 21:02:34 +0530

    Update readProperties.py

--- utlities/readProperties.py
+++ utlities/readProperties.py
@@ -1,7 +1,8 @@
 import configparser
 
 config = configparser.RawConfigParser()
-config.read(r"/home/gaurav/PycharmProjects/OrangeHRM-selenium-automation-testing-master/Configurations/config.ini")
+config.read("./Configurations/config.ini")
+# config.read("/home/gaurav/PycharmProjects/OrangeHRM-selenium-automation-testing-master/Configurations/config.ini")
 # config.read(r"C:\Users\DELL\selenium_work\OrangeHRM-selenium-automation-testing\Configurations\config.ini")
 
 
