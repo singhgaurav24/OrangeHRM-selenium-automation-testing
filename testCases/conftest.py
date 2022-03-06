a928ff9a0f134baa4938da542424bab705c78001
Gaurav Kumar Singh <55310399+singhgaurav24@users.noreply.github.com>
2022-03-06 20:53:43 +0530

    Update conftest.py

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -4,12 +4,23 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def setup():
     # driver = webdriver.Chrome(executable_path="F:\selemium\chromedriver.exe")
-    s = Service(ChromeDriverManager().install())
-#     s = Service('/home/gaurav/Downloads/chromedriver_linux64/chromedriver')
-    driver = webdriver.Chrome(service=s)
+
+    # s = Service(ChromeDriverManager().install())
+    # driver = webdriver.Chrome(service=s)
+
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+
     return driver
 
